5667d4ed61439ce9ea9fac0f3edadd75bb267913
joaomoura11 <134563387+joaomoura11@users.noreply.github.com>
2023-06-22 20:22:14 -0300

    Add files via upload

---
+++ Aluno/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    sexo = models.CharField(max_length=100)
+    matricula = models.CharField(max_length=100)
+    dataNascimento = models.DateField(max_length=100)
